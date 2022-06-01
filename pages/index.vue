78ef9d1ff250cb2c52fb19c1b8d03090ce864520
Brandon Mason <brandon@tlsft.com>
2022-06-01 16:05:04 -0600

    reset to generic project

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <script setup>
-  import {storeToRefs} from 'pinia'
-  import {useAuthStore} from "~/stores/auth"
-
-  const authStore = useAuthStore()
-  const {user, isLoggedIn} = storeToRefs(authStore)
 </script>
 
 <template>
-  <div>Welcome, {{user.fname}}!</div>
+  <div>Welcome, Human!</div>
 </template>
